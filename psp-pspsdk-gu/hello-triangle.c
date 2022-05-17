c6291dffe8edd482c74b3b2a25807c66dafca006
Brais Solla <brais.1997@gmail.com>
2022-05-17 18:51:52 +0200

    PSP demo now draws a triangle... Without colors

--- psp-pspsdk-gu/hello-triangle.c
+++ psp-pspsdk-gu/hello-triangle.c
@@ -42,46 +42,48 @@ static unsigned int __attribute__((aligned(16))) list[262144];
 int main(int argc, char* argv[]){
     setupCallbacks();
 
+    // Framebuffer pointer 0
+    void* fbp0 = getStaticVramBuffer(BUF_WIDTH,SCR_HEIGHT,GU_PSM_8888);
+
+    // Framebuffer pointer 1
+	void* fbp1 = getStaticVramBuffer(BUF_WIDTH,SCR_HEIGHT,GU_PSM_8888);
+
+    // Z Buffer pointer
+	void* zbp = getStaticVramBuffer(BUF_WIDTH,SCR_HEIGHT,GU_PSM_4444);
 
     // Init GU
     sceGuInit();
     sceGuStart(GU_DIRECT, list);
-    sceGuDrawBuffer(GU_PSM_8888, (void*) 0, BUF_WIDTH);
-    sceGuDispBuffer(SCR_WIDTH, SCR_HEIGHT, (void*) 0x88000, BUF_WIDTH);
-    sceGuDepthBuffer((void*)0x110000,BUF_WIDTH);
-	sceGuOffset(2048 - (SCR_WIDTH/2),2048 - (SCR_HEIGHT/2));
+
+    // Create buffers
+    sceGuDrawBuffer(GU_PSM_8888,fbp0,BUF_WIDTH);
+    sceGuDispBuffer(SCR_WIDTH,SCR_HEIGHT,fbp1,BUF_WIDTH);
+    sceGuDepthBuffer(zbp,BUF_WIDTH);
+    // PSP uses a virtual coordinate system [0-4096], translate to the middle of the screen
+    sceGuOffset(2048 - (SCR_WIDTH/2),2048 - (SCR_HEIGHT/2));
 	sceGuViewport(2048,2048,SCR_WIDTH,SCR_HEIGHT);
-	sceGuDepthRange(0xc350,0x2710);
-	sceGuScissor(0,0,SCR_WIDTH,SCR_HEIGHT);
+	sceGuDepthRange(65535,0);
+
+    // Show full screen
+    sceGuScissor(0,0,SCR_WIDTH,SCR_HEIGHT);
 	sceGuEnable(GU_SCISSOR_TEST);
-	sceGuFrontFace(GU_CW);
-	sceGuShadeModel(GU_SMOOTH);
-	sceGuEnable(GU_CULL_FACE);
-	sceGuEnable(GU_CLIP_PLANES);
 	sceGuFinish();
-	sceGuSync(0,0);
+
+    sceGuSync(0,0);
 	sceDisplayWaitVblankStart();
+    // Turn on display
 	sceGuDisplay(GU_TRUE);
 
 
-    float vertices[] = {
-        0.f, -.8f, 0.f, 0xff0000ff,
-        -.8f, .8f, 0.f, 0xff00ff00,
-        .8f, .8f,  0.f, 0xffff0000
-    };
 
-    uint32_t* colors = (uint32_t*) vertices;
-    // Hacky stuff
-    colors[3]  = 0xff0000ff;
-    colors[7]  = 0xff00ff00;
-    colors[11] = 0xffff0000;
 
     while(running()){
         sceGuStart(GU_DIRECT, list);
 
+        // Clear color and depth buffers
         sceGuClearColor(0);
         sceGuClearDepth(0);
-        // glClear-ish
+
         sceGuClear(GU_COLOR_BUFFER_BIT | GU_DEPTH_BUFFER_BIT);
 
         sceGumMatrixMode(GU_PROJECTION);
@@ -93,8 +95,23 @@ int main(int argc, char* argv[]){
         sceGumMatrixMode(GU_MODEL);
         sceGumLoadIdentity();
 
-        // Triangle vertices
-        sceGuDrawArray(GU_TRIANGLES, GU_VERTEX_32BITF | GU_COLOR_8888, 3*3, 0, vertices);
+        // Red?
+        sceGuColor(0xff0000ff);
+
+        // Allocate memory for the triangle vertices
+        struct Vertex* vertices = (struct Vertex*) sceGuGetMemory(3 * sizeof(struct Vertex));
+        // Generate the triangle
+        vertices[0].x = SCR_WIDTH / 2;
+		vertices[0].y = 20;
+		vertices[0].z = 0;
+		vertices[1].x = (SCR_WIDTH / 6);
+		vertices[1].y = (SCR_HEIGHT / 2) + 80;
+		vertices[1].z = 0;
+		vertices[2].x = SCR_WIDTH - (SCR_WIDTH / 6);
+		vertices[2].y = (SCR_HEIGHT / 2) + 80;
+		vertices[2].z = 0;
+
+        sceGuDrawArray(GU_TRIANGLES, GU_VERTEX_32BITF | GU_TRANSFORM_2D, 3, 0, vertices);
 
 
         sceGuFinish();
