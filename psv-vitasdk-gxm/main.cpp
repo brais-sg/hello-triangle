d979247f0989b7e7b569736311c98f16d66b177c
Brais Solla <brais.1997@gmail.com>
2022-05-22 20:05:36 +0200

    PSVITA baby steps

--- psv-vitasdk-gxm/main.cpp
+++ psv-vitasdk-gxm/main.cpp
@@ -18,12 +18,42 @@
 #include <psp2/kernel/processmgr.h>
 #include <psp2/kernel/threadmgr.h>
 
-int main(int argc, char* argv[]){
-    // Just wait for like 30 seconds and exit
+#include <psp2/gxm.h>
+#include <psp2/display.h>
+#include <psp2/ctrl.h>
 
-    for(int i = 0; i < 30; i++){
-        sceKernelDelayThread(1000000);
-    }
+#define DISPLAY_WIDTH 960
+#define DISPLAY_HEIGHT 544
+#define DISPLAY_STRIDE 1024
+#define DISPLAY_BUFFER_COUNT 2
+#define DISPLAY_COLOR_FORMAT SCE_GXM_COLOR_FORMAT_A8B8G8R8
+#define DISPLAY_PIXEL_FORMAT SCE_DISPLAY_PIXELFORMAT_A8B8G8R8
+#define MAX_PENDING_SWAPS (DISPLAY_BUFFER_COUNT - 1)
+
+struct display_queue_callback_data {
+	void *addr;
+};
+
+static void display_queue_callback(const void *callbackData);
+
+// Before doing nothing: Take a look at xerpi's gxmfun repository
+int main(int argc, char* argv[]){
+    SceGxmInitializeParams gxm_init_params;
+    memset(&gxm_init_params, 0, sizeof(gxm_init_params));
+
+    gxm_init_params.flags = 0;
+    gxm_init_params.displayQueueMaxPendingCount = MAX_PENDING_SWAPS;
+    gxm_init_params.displayQueueCallback = display_queue_callback;
+    gxm_init_params.displayQueueCallbackDataSize = sizeof(struct display_queue_callback_data);
+    gxm_init_params.parameterBufferSize = SCE_GXM_DEFAULT_PARAMETER_BUFFER_SIZE;
+
+    sceGxmInitialize(&gxm_init_params);
+
+    // Initialize GPU rings
+    
+
+
+
 
     sceKernelExitProcess(0);
     return 0;
